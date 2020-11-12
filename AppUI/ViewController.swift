823c43cfff85e097932fff080798490ec642f0c7
MeetBudheliya <meetbudheliya04@gmail.com>
2020-11-12 19:05:38 +0530

    12/11/2020

--- AppUI/ViewController.swift
+++ AppUI/ViewController.swift
@@ -19,12 +19,12 @@ class ViewController: UIViewController ,UITextFieldDelegate{
     @IBOutlet weak var sixthCollection: UICollectionView!
     @IBOutlet weak var coinsBTN: UIButton!
     @IBOutlet weak var contentView: UIView!
-    var segmentMenu = ["RECOMMENDED","RANK","CATEGORY","BEST OF THE MONTH"]
+    var segmentMenu = ["RECOMMENDED","RANK","CATEGORY"]
     var imgList:[UIImage] = [#imageLiteral(resourceName: "img2"),#imageLiteral(resourceName: "img1"),#imageLiteral(resourceName: "img3"),#imageLiteral(resourceName: "img2"),#imageLiteral(resourceName: "img1"),#imageLiteral(resourceName: "img3")]
     //    var imgList1:[UIImage] = [#imageLiteral(resourceName: "bg4"),#imageLiteral(resourceName: "bg3"),#imageLiteral(resourceName: "bg2"),#imageLiteral(resourceName: "bg5"),#imageLiteral(resourceName: "bg1"),#imageLiteral(resourceName: "bg5")]
     var app:[appdata] = [appdata(img:[#imageLiteral(resourceName: "SnapChat"),#imageLiteral(resourceName: "Instagram"),#imageLiteral(resourceName: "Twitter"),#imageLiteral(resourceName: "Facebook"),#imageLiteral(resourceName: "Messenger")] , appName: ["SnapChat","Instagram","Twitter","Facebook","Messenger"], rate: ["4.8","4.9","4.3","4.7","4.1"], size:                             ["25.1M","33.8M","21.16M","20.5M","851K"]),
-                        appdata(img: [#imageLiteral(resourceName: "Flipcart"),#imageLiteral(resourceName: "Snapdeal"),#imageLiteral(resourceName: "OLX"),#imageLiteral(resourceName: "Swiggy"),#imageLiteral(resourceName: "Amazon"),#imageLiteral(resourceName: "shopee"),#imageLiteral(resourceName: "ShopNow")], appName: ["Flipcart","Snapdeal","OLX","Swiggy","Amazon","ShopNow","Shoppers"], rate: ["3.9","4.1","3.4","4.2","4.9","4.6","4.0"], size: ["5M","74.2M","84M","126M","29.3M","124M","29.5M"]),
-    appdata(img: [#imageLiteral(resourceName: "SubwaySurfers"),#imageLiteral(resourceName: "LudoKing"),#imageLiteral(resourceName: "Pubg"),#imageLiteral(resourceName: "GTA5"),#imageLiteral(resourceName: "SanAndreas"),#imageLiteral(resourceName: "Chess")], appName: ["Subway Surfers","LudoKing","PubG","GTA5","SanAndreas","Chess"], rate: ["4.7","4.9","2.9","4.1","3.5","3.8"], size: ["124M","38M","2.9G","1.7G","1.9G","125M"])]
+                         appdata(img: [#imageLiteral(resourceName: "Flipcart"),#imageLiteral(resourceName: "Snapdeal"),#imageLiteral(resourceName: "OLX"),#imageLiteral(resourceName: "Swiggy"),#imageLiteral(resourceName: "Amazon"),#imageLiteral(resourceName: "shopee"),#imageLiteral(resourceName: "ShopNow")], appName: ["Flipcart","Snapdeal","OLX","Swiggy","Amazon","ShopNow","Shoppers"], rate: ["3.9","4.1","3.4","4.2","4.9","4.6","4.0"], size: ["5M","74.2M","84M","126M","29.3M","124M","29.5M"]),
+                         appdata(img: [#imageLiteral(resourceName: "SubwaySurfers"),#imageLiteral(resourceName: "LudoKing"),#imageLiteral(resourceName: "Pubg"),#imageLiteral(resourceName: "GTA5"),#imageLiteral(resourceName: "SanAndreas"),#imageLiteral(resourceName: "Chess")], appName: ["Subway Surfers","LudoKing","PubG","GTA5","SanAndreas","Chess"], rate: ["4.7","4.9","2.9","4.1","3.5","3.8"], size: ["124M","38M","2.9G","1.7G","1.9G","125M"])]
     var data = coll5Data(color: [#colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1),#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1),#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1),#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1),#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1),#colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1),#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1),#colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)],img: [#imageLiteral(resourceName: "Amazon"),#imageLiteral(resourceName: "Flipcart"),#imageLiteral(resourceName: "Pubg"),#imageLiteral(resourceName: "LudoKing"),#imageLiteral(resourceName: "Instagram"),#imageLiteral(resourceName: "Amazon"),#imageLiteral(resourceName: "Flipcart"),#imageLiteral(resourceName: "Pubg"),#imageLiteral(resourceName: "LudoKing"),#imageLiteral(resourceName: "Instagram")], lbl: ["Amazon","Flipcart","Pubg","LudoKing","Instagram","Amazon","Flipcart","Pubg","LudoKing","Instagram"])
     var sixthData = coll6Data(color: [#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1),#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1),#colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1),#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1),#colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1),#colorLiteral(red: 0.5058823824, green: 0.3372549117, blue: 0.06666667014, alpha: 1)], lbl: ["Entertainment","Tools","Communication","Action","Racing","Adventure"], img1: [#imageLiteral(resourceName: "Flipcart"),#imageLiteral(resourceName: "Twitter"),#imageLiteral(resourceName: "LinkedIn"),#imageLiteral(resourceName: "SubwaySurfers"),#imageLiteral(resourceName: "Pubg"),#imageLiteral(resourceName: "LudoKing")], img2: [#imageLiteral(resourceName: "ShopNow"),#imageLiteral(resourceName: "Facebook"),#imageLiteral(resourceName: "Instagram"),#imageLiteral(resourceName: "Chess"),#imageLiteral(resourceName: "Snapdeal"),#imageLiteral(resourceName: "OLX")], img3: [#imageLiteral(resourceName: "shopee"),#imageLiteral(resourceName: "Facebook"),#imageLiteral(resourceName: "OLX"),#imageLiteral(resourceName: "ShopNow"),#imageLiteral(resourceName: "Swiggy"),#imageLiteral(resourceName: "Twitter")])
     
@@ -34,16 +34,17 @@ class ViewController: UIViewController ,UITextFieldDelegate{
         startTimer()
         coinsBTN.layer.cornerRadius = coinsBTN.frame.height/2
         coinsBTN.layer.masksToBounds = true
-//        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
-//            view.addGestureRecognizer(tap)
-
+        navigationController?.navigationBar.isHidden = true
+        //        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        //            view.addGestureRecognizer(tap)
+        
     }
-//   func dismissKeyboard() {
-//        //Causes the view (or one of its embedded text fields) to resign the first responder status.
-//       // view.endEditing(true)
-//        searchBar.searchTextField.resignFirstResponder()
-//    }
-//
+    //   func dismissKeyboard() {
+    //        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+    //       // view.endEditing(true)
+    //        searchBar.searchTextField.resignFirstResponder()
+    //    }
+    //
     func startTimer() {
         Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(scrollToNextCell), userInfo: nil, repeats: true);
     }
@@ -124,11 +125,11 @@ extension ViewController:UICollectionViewDelegate,UICollectionViewDataSource
             cell.backIMG.isHidden = false
             cell.backIMG.layer.cornerRadius = 25
             cell.backIMG.image = #imageLiteral(resourceName: "bg4")
-                        cell.img.layer.cornerRadius = 15
-                        cell.img.image = app[2].img[indexPath.row]
-                        cell.nameLBL.text = app[2].appName[indexPath.row]
-                        cell.rateBTN.titleLabel?.text = app[2].rate[indexPath.row]
-                        cell.sizeLBL.text = app[2].size[indexPath.row]
+            cell.img.layer.cornerRadius = 15
+            cell.img.image = app[2].img[indexPath.row]
+            cell.nameLBL.text = app[2].appName[indexPath.row]
+            cell.rateBTN.titleLabel?.text = app[2].rate[indexPath.row]
+            cell.sizeLBL.text = app[2].size[indexPath.row]
             cell.btn.setTitle("Play", for: .normal)
             return cell
         case fifthCollection:
@@ -172,7 +173,7 @@ extension ViewController:UICollectionViewDelegate,UICollectionViewDataSource
             }
         }
     }
-
+    
 }
 class FirstCellOfMain:UICollectionViewCell
 {
--- AppUI/secondVC.swift
+++ AppUI/secondVC.swift
@@ -8,31 +8,61 @@
 import UIKit
 
 class secondVC: UIViewController ,UITableViewDelegate,UITableViewDataSource{
-
+    
     @IBOutlet weak var segmentControll: UISegmentedControl!
     @IBOutlet weak var tblView: UITableView!
-    var data:[tbldata] = [tbldata(name: ["Twitter","OLX","Amazon","Flipcart","Messenger","Instagram","Snapdeal","Swiggy","Snapchat"], img: [#imageLiteral(resourceName: "Twitter"),#imageLiteral(resourceName: "OLX"),#imageLiteral(resourceName: "Amazon"),#imageLiteral(resourceName: "Flipcart"),#imageLiteral(resourceName: "Messenger"),#imageLiteral(resourceName: "Instagram"),#imageLiteral(resourceName: "Snapdeal"),#imageLiteral(resourceName: "Swiggy"),#imageLiteral(resourceName: "SnapChat")], numberIMG: [UIImage(systemName:"1.circle.fill"),UIImage(systemName: "2.circle.fill"),UIImage(systemName: "3.circle.fill"),UIImage(systemName: "4.circle.fill"),UIImage(systemName: "5.circle.fill"),UIImage(systemName: "6.circle.fill"),UIImage(systemName: "7.circle.fill"),UIImage(systemName: "8.circle.fill"),UIImage(systemName: "9.circle.fill")], rating: ["3.9","4.2","2.9","3.9","4.6","3.9","4.7","3.8","4.2"], size: ["25M","39M","12M","46M","120M","79M","12M","56M","78.1M"], category: ["Social Media App","Shopping","Shopping","Shopping","Messeging App","Social Media App","Shoppping","Food Delivery","Social Media App"])]
+    var data:[tbldata] = [tbldata(name: ["Twitter","OLX","Amazon","Flipcart","Messenger","Instagram","Snapdeal","Swiggy","Snapchat"], img: [#imageLiteral(resourceName: "Twitter"),#imageLiteral(resourceName: "OLX"),#imageLiteral(resourceName: "Amazon"),#imageLiteral(resourceName: "Flipcart"),#imageLiteral(resourceName: "Messenger"),#imageLiteral(resourceName: "Instagram"),#imageLiteral(resourceName: "Snapdeal"),#imageLiteral(resourceName: "Swiggy"),#imageLiteral(resourceName: "SnapChat")], number:["1","2","3","4","5","6","7","8","9"], rating: ["3.9","4.2","2.9","3.9","4.6","3.9","4.7","3.8","4.2"], size: ["25M","39M","12M","46M","120M","79M","12M","56M","78.1M"], category: ["Social Media App","Shopping","Shopping","Shopping","Messeging App","Social Media App","Shoppping","Food Delivery","Social Media App"]),
+                          tbldata(name: ["LudoKing","PubG","GTA5","Subway Surfers","Chess","San Andreas","Snak Game","GTA5","PubG"], img: [#imageLiteral(resourceName: "LudoKing"),#imageLiteral(resourceName: "Pubg"),#imageLiteral(resourceName: "GTA5"),#imageLiteral(resourceName: "SubwaySurfers"),#imageLiteral(resourceName: "Chess"),#imageLiteral(resourceName: "SanAndreas"),#imageLiteral(resourceName: "LudoKing"),#imageLiteral(resourceName: "GTA5"),#imageLiteral(resourceName: "Pubg")], number:["1","2","3","4","5","6","7","8","9"], rating: ["3.9","4.2","2.9","3.9","4.6","3.9","4.7","4.6","4.2"], size: ["25M","556M","12M","46M","120M","79M","12M","1.4G","2.5G"], category: ["Most Rated","Action","Action & Travelling","Running","Mind Game","Racing & Action","Most Played","Action","Action % Thrilling"])]
     override func viewDidLoad() {
         super.viewDidLoad()
+        segmentControll.setTitle("Apps", forSegmentAt: 0)
+        segmentControll.setTitle("Games", forSegmentAt: 1)
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       
+        if segmentControll.selectedSegmentIndex == 0
+        {
             return data[0].name.count
-       
+            
+        }
+        else
+        {
+            return data[1].name.count
+        }
+        
+        
         
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tblView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath) as! tableCell
-       
-            cell.rankBTN.setBackgroundImage(data[0].numberIMG[indexPath.row] , for: .highlighted)
+        if segmentControll.selectedSegmentIndex == 0
+        {
+            
+            cell.rankBTN.setTitle(data[0].number[indexPath.row], for: .normal)
             cell.img.image = data[0].img[indexPath.row]
             cell.name.text = data[0].name[indexPath.row]
             cell.rating.setTitle(data[0].rating[indexPath.row], for: .normal)
             cell.sizeLBL.text = data[0].size[indexPath.row]
+            cell.category.text = data[0].category[indexPath.row]
             cell.btn.setTitle("GET", for: .normal)
+        }
+        else{
+         
+            cell.rankBTN.setTitle(data[1].number[indexPath.row], for: .normal)
+            cell.img.image = data[1].img[indexPath.row]
+            cell.name.text = data[1].name[indexPath.row]
+            cell.rating.setTitle(data[1].rating[indexPath.row], for: .normal)
+            cell.sizeLBL.text = data[1].size[indexPath.row]
+            cell.category.text = data[1].category[indexPath.row]
+            cell.btn.setTitle("PLAY", for: .normal)
             
-       
+        }
         return cell
+    }
+    @IBAction func segmentChange(_ sender: UISegmentedControl) {
+        tblView.reloadData()
+    }
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        print("Pressed")
     }
 }
 class tableCell: UITableViewCell {
@@ -52,7 +82,7 @@ class tableCell: UITableViewCell {
 struct tbldata {
     var name:[String]
     var img:[UIImage]
-    var numberIMG:[UIImage?]
+    var number:[String]
     var rating:[String]
     var size:[String]
     var category:[String]
