79c9735ad874e7d62179f6f7c77b501490b6a59e
MeetBudheliya <meetbudheliya04@gmail.com>
2020-11-19 18:59:16 +0530

    Commit

--- AppUI/mainTabs/MeVC.swift
+++ AppUI/mainTabs/MeVC.swift
@@ -13,12 +13,29 @@ class MeVC: UIViewController {
     @IBOutlet weak var mainAppCollection: UICollectionView!
     @IBOutlet weak var adsCollection: UICollectionView!
     var data = Data(img: [#imageLiteral(resourceName: "Share Files"),#imageLiteral(resourceName: "Battery Usage"),#imageLiteral(resourceName: "Security"),#imageLiteral(resourceName: "Whatsapp Cleaner"),#imageLiteral(resourceName: "Cleaner-1"),#imageLiteral(resourceName: "Uninnstall  Apps"),#imageLiteral(resourceName: "Dual  Apps"),#imageLiteral(resourceName: "Settings")], name: ["Share Files","Battery Usage","Secure Files","Whatsapp Cleaner","Trash Cleaner","App Uninstall","Security Scan","Settings"], dis:["","89%","","356M","","2 Unused","",""])
-    var imgData:[UIImage] = [#imageLiteral(resourceName: "bg1"),#imageLiteral(resourceName: "bg1"),#imageLiteral(resourceName: "bg1"),#imageLiteral(resourceName: "bg1"),#imageLiteral(resourceName: "bg1")]
+    var imgData:[UIImage] = [#imageLiteral(resourceName: "banner-ad2-728x90"),#imageLiteral(resourceName: "horizontal-ad-banner-ready-web-sites-press-as-discount-advertisement-flyer-contains-black-friday-sale-black-friday-sale-103627128"),#imageLiteral(resourceName: "images"),#imageLiteral(resourceName: "bn_bg_wide"),#imageLiteral(resourceName: "Google-Ads-logo-horizontal")]
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         mainAppCollection.reloadData()
         updateBTN.layer.cornerRadius = 10
-        adsCollection.register(UINib(nibName: "ADScollectionCell", bundle: nil), forCellWithReuseIdentifier: "adCell")
+    //    adsCollection.register(UINib(nibName: "firstCollection", bundle: nil), forCellWithReuseIdentifier: "firstCollectionCell")
+        startTimer()
+    }
+    func startTimer() {
+        Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(scrollToNextCell), userInfo: nil, repeats: true);
+    }
+    var x = 0
+    
+    @objc func scrollToNextCell(){
+        if self.x < imgData.count {
+            let indexPath = IndexPath(item: x, section: 0)
+            self.adsCollection.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+            self.x = self.x + 1
+        }else{
+            self.x = 0
+            self.adsCollection.scrollToItem(at: IndexPath(item: 0, section: 0), at: .centeredHorizontally, animated: true)
+        }
     }
     @IBAction func updateBTNClicked(_ sender: UIButton) {
         print("Clicked")
@@ -51,7 +68,7 @@ extension MeVC:UICollectionViewDelegate,UICollectionViewDataSource
         }
         else
         {
-            let cell = adsCollection.dequeueReusableCell(withReuseIdentifier: "adCell", for: indexPath) as! ADScollectionCell
+            let cell = adsCollection.dequeueReusableCell(withReuseIdentifier: "CollectionCell", for: indexPath) as! ADScollectionCell
             cell.img.image = imgData[indexPath.row]
             return cell
         }
@@ -66,6 +83,17 @@ class collectionCell:UICollectionViewCell
     @IBOutlet weak var discription: UILabel!
     
 }
+
+class ADScollectionCell: UICollectionViewCell {
+    
+    @IBOutlet weak var img: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        img.layer.cornerRadius = 10
+    }
+
+}
+
 struct Data {
     var img:[UIImage]
     var name:[String]
